273a855d941869436ce8f1e94cdd3b0f8835b943
zzlzlizhen <275290501@qq.com>
2019-08-08 14:12:31 +0800

    提交

---
+++ zkwl-admin/src/main/java/com/remote/modules/device/entity/DeviceTree.java
@@ -0,0 +1,27 @@
+package com.remote.modules.device.entity;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @Author zhagnwenping
+ * @Date 2019/8/7 11:29
+ * @Version 1.0
+ **/
+@Data
+public class DeviceTree {
+    private String id;
+
+    private String name;
+
+    private Integer runState;
+
+    private Integer deviceNumber;
+
+    private Integer projectStatus;
+
+    private List<DeviceTree> children;
+
+    private String parentId;
+}
